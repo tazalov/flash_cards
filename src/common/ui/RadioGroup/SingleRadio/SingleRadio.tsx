ee2a03f80e7f794983aa6044bb1ee2439b7fd627
ruslan <zrm1306@gmail.com>
2023-12-12 18:26:20 +0500

    fix: fix styles

--- src/common/ui/RadioGroup/SingleRadio/SingleRadio.tsx
+++ src/common/ui/RadioGroup/SingleRadio/SingleRadio.tsx
@@ -1,5 +1,6 @@
 import { ComponentPropsWithoutRef } from 'react'
 
+import { TypographyVariant } from '@/common/enums'
 import { Typography } from '@/common/ui/Typography'
 import * as RadixRadio from '@radix-ui/react-radio-group'
 import cn from 'classnames'
@@ -8,16 +9,18 @@ import s from './SingleRadio.module.scss'
 
 export type SingleRadioProps = {
   className?: string
-  title: string
+  label: string
 } & Omit<ComponentPropsWithoutRef<typeof RadixRadio.Item>, 'asChild'>
 
-export const SingleRadio = ({ className, disabled, title, ...rest }: SingleRadioProps) => {
+export const SingleRadio = ({ className, disabled, label, ...rest }: SingleRadioProps) => {
   return (
     <Typography as="label" className={cn(s.labelWrapper, { [s.disabled]: disabled }, className)}>
       <RadixRadio.Item className={s.item} disabled={disabled} {...rest}>
         <RadixRadio.Indicator className={s.indicator} />
       </RadixRadio.Item>
-      <div className={s.title}>{title}</div>
+      <Typography as="span" className={s.label} variant={TypographyVariant.body2}>
+        {label}
+      </Typography>
     </Typography>
   )
 }
