e0a4a6bf96a3bb02621748153e6fd57e68257054
ruslan <zrm1306@gmail.com>
2023-12-14 16:27:38 +0500

    feat: add component and stories for modals

---
+++ src/common/ui/Modals/Modals.tsx
@@ -0,0 +1,47 @@
+import { ComponentProps, ReactNode } from 'react'
+
+import Cross from '@/common/assets/icons/cross'
+import { TypographyVariant } from '@/common/enums'
+import { Card } from '@/common/ui/Card'
+import { Typography } from '@/common/ui/Typography'
+import * as RadixModal from '@radix-ui/react-dialog'
+import cn from 'classnames'
+
+import s from './Modals.module.scss'
+
+export type Props = {
+  open: boolean
+  setOpenModal: (open: boolean) => void
+  title?: string
+  trigger: ReactNode
+} & ComponentProps<'div'>
+
+export const Modal = (props: Props) => {
+  const { children, className, open, setOpenModal, title, trigger } = props
+
+  return (
+    <RadixModal.Root onOpenChange={setOpenModal} open={open}>
+      <RadixModal.Trigger asChild>{trigger}</RadixModal.Trigger>
+      {open && (
+        <RadixModal.Portal forceMount>
+          <RadixModal.Overlay className={s.overlay} />
+          <div className={cn(s.main, className)}>
+            <RadixModal.Content>
+              <Card className={s.card}>
+                {title && (
+                  <div className={s.title}>
+                    <Typography variant={TypographyVariant.h2}>{title}</Typography>
+                    <RadixModal.Close asChild>
+                      <Cross fill="#FFF" style={{ cursor: 'pointer' }} />
+                    </RadixModal.Close>
+                  </div>
+                )}
+                <div className={s.content}>{children}</div>
+              </Card>
+            </RadixModal.Content>
+          </div>
+        </RadixModal.Portal>
+      )}
+    </RadixModal.Root>
+  )
+}
