696645bf73c26799b6ebc81a7b61b3bc1355ac73
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2023-12-16 12:59:59 +0300

    #24 feat: add Slider stories

---
+++ src/common/ui/Slider/Slider.stories.tsx
@@ -0,0 +1,55 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { ComponentPropsWithoutRef, useRef, useState } from 'react'
+
+import * as RadixSlider from '@radix-ui/react-slider'
+
+import { Slider } from './Slider'
+
+const meta = {
+  argTypes: {
+    asChild: {
+      control: false,
+    },
+  },
+  component: Slider,
+  tags: ['autodocs'],
+  title: 'Components/Slider',
+} satisfies Meta<typeof Slider>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const DefaultWithoutInput: Story = {
+  args: {
+    isInputControl: false,
+    value: [0, 100],
+  },
+}
+
+const RenderComp = (props: ComponentPropsWithoutRef<typeof RadixSlider.Root>) => {
+  const [values, setValues] = useState(props.value)
+  const ref = useRef<any>()
+
+  return <Slider {...props} onValueChange={setValues} ref={ref} value={values} />
+}
+
+export const SliderWithoutInputControl: Story = {
+  args: {
+    isInputControl: false,
+    max: 100,
+    min: 0,
+    step: 1,
+    value: [0, 100],
+  },
+  render: args => <RenderComp {...args} />,
+}
+export const SliderWithInputControl: Story = {
+  args: {
+    max: 100,
+    min: 0,
+    step: 1,
+    value: [0, 100],
+  },
+  render: args => <RenderComp {...args} />,
+}
