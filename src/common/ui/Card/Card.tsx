70b01e3f7e224cd4d5cadd66e9ab75acf92eee16
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2023-12-18 15:35:13 +0300

    #43 fix: change types in Card component

--- src/common/ui/Card/Card.tsx
+++ src/common/ui/Card/Card.tsx
@@ -1,37 +1,24 @@
-import { ComponentPropsWithoutRef, ElementRef, ElementType, forwardRef } from 'react'
+import { ComponentPropsWithoutRef, ElementRef, ElementType, ReactNode, forwardRef } from 'react'
 
+import { PolymorphRef } from '@/common/types/intex'
 import cn from 'classnames'
 
 import s from './Card.module.scss'
 
 type Props<T extends ElementType> = {
   as?: T
-  className?: string
-  height?: string
-  width?: string
 } & ComponentPropsWithoutRef<T>
 
-export const Card = forwardRef(
+type CardComponent = <T extends ElementType = 'button'>(
+  props: Props<T> & PolymorphRef<T>
+) => ReactNode
+export const Card: CardComponent = forwardRef(
   <T extends ElementType = 'div'>(
-    {
-      as,
-      className,
-      height,
-      style,
-      width,
-      ...restProps
-    }: Props<T> & Omit<ComponentPropsWithoutRef<T>, keyof Props<T>>,
+    { as, className, ...restProps }: Props<T>,
     ref: ElementRef<T>
   ) => {
     const Component: ElementType = as || 'div'
 
-    return (
-      <Component
-        className={cn(s.card, className)}
-        {...restProps}
-        ref={ref}
-        style={{ ...style, height, width }}
-      />
-    )
+    return <Component className={cn(s.card, className)} ref={ref} {...restProps} />
   }
 )
