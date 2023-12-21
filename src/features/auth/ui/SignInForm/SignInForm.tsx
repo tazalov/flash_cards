56b561a59960af7df0fab15b4224ead48f223a09
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2023-12-21 13:31:51 +0300

    refactor: add as='header' to typography

--- src/features/auth/ui/SignInForm/SignInForm.tsx
+++ src/features/auth/ui/SignInForm/SignInForm.tsx
@@ -22,7 +22,7 @@ export const SignInForm = ({ className, onSubmit }: Props) => {
 
   return (
     <Card as="form" className={cn(s.form, className)} onSubmit={handleSubmit(onSubmit)}>
-      <Typography className={s.headerText} variant={TypographyVariant.large}>
+      <Typography as="header" className={s.headerText} variant={TypographyVariant.large}>
         Sign In
       </Typography>
       <ControlledTextField
