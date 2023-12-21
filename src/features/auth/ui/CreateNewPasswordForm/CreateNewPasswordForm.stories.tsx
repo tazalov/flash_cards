cd8f696abf2f3c8d0cf45c1b8d4bb03dd18a165e
ruslan <zrm1306@gmail.com>
2023-12-21 19:46:16 +0500

    fix: styles and add action on onSubmit

--- src/features/auth/ui/CreateNewPasswordForm/CreateNewPasswordForm.stories.tsx
+++ src/features/auth/ui/CreateNewPasswordForm/CreateNewPasswordForm.stories.tsx
@@ -1,11 +1,12 @@
 import { Meta, StoryObj } from '@storybook/react'
 
 import { CreateNewPasswordForm } from './'
-import { CreateNewPasswordFormValues } from './useCreateNewPasswordForm'
 
 const meta = {
-  args: {
-    onSubmit: (data: CreateNewPasswordFormValues) => console.log(data),
+  argTypes: {
+    onSubmit: {
+      action: 'Create new password',
+    },
   },
   component: CreateNewPasswordForm,
   tags: ['autodocs'],
