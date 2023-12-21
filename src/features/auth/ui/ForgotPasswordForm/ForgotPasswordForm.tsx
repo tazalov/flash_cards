2c9e64db3a063b287e0d15d8af9670d844e1c589
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2023-12-21 13:39:19 +0300

    refactor: add as='header' to typography

--- src/features/auth/ui/ForgotPasswordForm/ForgotPasswordForm.tsx
+++ src/features/auth/ui/ForgotPasswordForm/ForgotPasswordForm.tsx
@@ -24,7 +24,7 @@ export const ForgotPasswordForm = ({ className, onSubmit }: Props) => {
 
   return (
     <Card as="form" className={cn(s.form, className)} onSubmit={handleSubmit(onSubmit)}>
-      <Typography className={s.headerText} variant={TypographyVariant.large}>
+      <Typography as="header" className={s.headerText} variant={TypographyVariant.large}>
         Forgot your password?
       </Typography>
       <ControlledTextField
