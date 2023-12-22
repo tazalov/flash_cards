cc30ac661a13fd686aff7f12502997caab91fec0
ruslan <zrm1306@gmail.com>
2023-12-22 21:21:07 +0500

    fix: style for form

--- src/features/auth/ui/CreateNewPasswordForm/CreateNewPasswordForm.tsx
+++ src/features/auth/ui/CreateNewPasswordForm/CreateNewPasswordForm.tsx
@@ -16,25 +16,24 @@ export const CreateNewPasswordForm = ({ onSubmit }: Props) => {
   const { control, errors, handleSubmit } = useCreateNewPasswordForm()
 
   return (
-    <Card className={s.formContent}>
-      <Typography className={s.formTitle} variant={TypographyVariant.large}>
+    <Card as="form" className={s.formContent} onSubmit={handleSubmit(onSubmit)}>
+      <Typography as="h2" className={s.formTitle} variant={TypographyVariant.large}>
         Create new password
       </Typography>
-      <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
-        <ControlledTextField
-          control={control}
-          errorText={errors.password?.message}
-          label="Password"
-          name="password"
-          type="password"
-        />
-        <Typography className={s.formInfoText} variant={TypographyVariant.body2}>
-          Create new password and we will send you further instructions to email
-        </Typography>
-        <Button fullWidth type="submit">
-          Create New Password
-        </Button>
-      </form>
+      <ControlledTextField
+        className={s.input}
+        control={control}
+        errorText={errors.password?.message}
+        label="Password"
+        name="password"
+        type="password"
+      />
+      <Typography className={s.formInfoText} variant={TypographyVariant.body2}>
+        Create new password and we will send you further instructions to email
+      </Typography>
+      <Button fullWidth type="submit">
+        Create New Password
+      </Button>
     </Card>
   )
 }
