fb9febc8498d846821001b8ce47ffa81bcaaa886
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2023-12-22 12:36:55 +0300

    feat: add CheckEmail component

---
+++ src/features/auth/ui/CheckEmail/CheckEmail.tsx
@@ -0,0 +1,35 @@
+import { TypographyVariant } from '@/common/enums'
+import { Avatar } from '@/common/ui/Avatar'
+import { Button } from '@/common/ui/Button'
+import { Card } from '@/common/ui/Card'
+import { Typography } from '@/common/ui/Typography'
+import cn from 'classnames'
+
+import s from './CheckEmail.module.scss'
+
+import checkEmail from '../../../../common/assets/image/checkEmail.svg'
+
+interface Props {
+  className?: string
+  email: string
+}
+export const CheckEmail = ({ className, email }: Props) => {
+  return (
+    <Card className={cn(s.form, className)}>
+      <Typography className={s.headerText} variant={TypographyVariant.large}>
+        Check Email
+      </Typography>
+      <Avatar className={s.image} src={checkEmail} title="" />
+      <Typography
+        className={s.descriptionText}
+        textAlign="center"
+        variant={TypographyVariant.body2}
+      >
+        We’ve sent an Email with instructions to {email}
+      </Typography>
+      <Button as="a" className={s.signInNav} fullWidth>
+        Back to Sign In
+      </Button>
+    </Card>
+  )
+}
