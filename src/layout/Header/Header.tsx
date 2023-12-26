4cb905aca4bef0a0ef9c0c9e7f8dff1834e8d907
tazalov <tazalov9@gmail.com>
2023-12-26 10:09:31 +0300

    fix: header styles

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,26 +1,30 @@
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
+
 import Logo from '@/common/assets/images/logo.png'
 import { ProfileData } from '@/common/types'
 import { Button } from '@/common/ui/Button'
+import { Page } from '@/common/ui/Page'
 import cn from 'classnames'
 
 import s from './Header.module.scss'
 
 import { HeaderUser } from './HeaderUser'
 
-interface Props {
-  className?: string
+type Props = {
   data?: ProfileData
   isAuth: boolean
   logout: () => void
-}
+} & ComponentPropsWithoutRef<'header'>
 
-export const Header = ({ className, data, isAuth, logout }: Props) => {
-  return (
-    <div className={cn(s.header, className)}>
-      <div className={s.container}>
-        <img alt="logo" className={s.logo} src={Logo} />
-        {isAuth && data ? <HeaderUser data={data} logout={logout} /> : <Button>Sign In</Button>}
-      </div>
-    </div>
-  )
-}
+export const Header = forwardRef<ElementRef<'header'>, Props>(
+  ({ className, data, isAuth, logout, ...rest }, ref) => {
+    return (
+      <header className={cn(s.header, className)} ref={ref} {...rest}>
+        <Page className={s.container} mt={0}>
+          <img alt="logo" className={s.logo} src={Logo} />
+          {isAuth && data ? <HeaderUser data={data} logout={logout} /> : <Button>Sign In</Button>}
+        </Page>
+      </header>
+    )
+  }
+)
