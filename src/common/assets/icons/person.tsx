aa083a7947cd1db7fd6c4e35e166a05219e9f698
tazalov <tazalov9@gmail.com>
2023-12-26 10:11:02 +0300

    fix: icon ref

--- src/common/assets/icons/person.tsx
+++ src/common/assets/icons/person.tsx
@@ -1,8 +1,9 @@
-import { SVGProps, forwardRef, memo } from 'react'
-const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
+import { Ref, SVGProps, forwardRef, memo } from 'react'
+const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
   <svg
     fill="currentColor"
     height={18}
+    ref={ref}
     viewBox="0 0 16 16"
     width={18}
     xmlns="http://www.w3.org/2000/svg"
