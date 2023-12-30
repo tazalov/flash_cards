66d56b722f36e47b5be73705eb565371b4635b03
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2023-12-30 12:18:38 +0300

    feat: added DecksList page to privateRoutes

--- src/app/routes/config/router.tsx
+++ src/app/routes/config/router.tsx
@@ -2,10 +2,11 @@ import { RouteObject, createBrowserRouter } from 'react-router-dom'
 
 import { ProtectedAuth } from '@/app/routes/ui/ProtectedAuth'
 import { MainLayout } from '@/layout/MainLayout'
+import { DecksList } from '@/pages/Decks/DecksList'
 
 const publicRoutes: RouteObject[] = []
 
-const privateRoutes: RouteObject[] = []
+const privateRoutes: RouteObject[] = [{ element: <DecksList />, path: '/' }]
 
 const appRoutes: RouteObject[] = [
   {
