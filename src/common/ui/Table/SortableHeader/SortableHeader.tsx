69f0c6cf37321913777aab7d9b2706969d6d27e1
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2023-12-30 12:02:59 +0300

    feat: add sortableHeader component to Table

---
+++ src/common/ui/Table/SortableHeader/SortableHeader.tsx
@@ -0,0 +1,55 @@
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
+
+import { Column, Sort } from '@/common/types'
+import cn from 'classnames'
+
+import s from './SortableHeader.module.scss'
+
+import { Table } from '../Table'
+
+type HeaderWithSortProps = {
+  columns: Column[]
+  onSort?: (sort: Sort) => void
+  sort?: Sort
+} & Omit<ComponentPropsWithoutRef<'thead'>, 'children'>
+
+export const SortableHeader = forwardRef<ElementRef<'thead'>, HeaderWithSortProps>(
+  ({ columns, onSort, sort, ...restProps }, ref) => {
+    const handleSort = (key: string, sortable?: boolean) => () => {
+      if (!onSort || !sortable) {
+        return
+      }
+
+      const currentDirection = sort?.key === key ? sort?.direction : null
+
+      switch (currentDirection) {
+        case 'asc': {
+          return onSort({ direction: 'desc', key })
+        }
+        case 'desc': {
+          return onSort(null)
+        }
+        default: {
+          return onSort({ direction: 'asc', key })
+        }
+      }
+    }
+
+    return (
+      <Table.Head {...restProps} ref={ref}>
+        <Table.Row>
+          {columns.map(({ key, sortable, title }) => (
+            <Table.TitleCell
+              className={cn({ [s.headCell]: sortable })}
+              key={key}
+              onClick={handleSort(key, sortable)}
+            >
+              {title}
+              {sort && sort.key === key && <span>{sort.direction === 'asc' ? ' ▲' : ' ▼'}</span>}
+            </Table.TitleCell>
+          ))}
+        </Table.Row>
+      </Table.Head>
+    )
+  }
+)
