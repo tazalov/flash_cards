f6ddca713ba00b0361eaf08c34323ef6dc9a2f8e
tazalov <tazalov9@gmail.com>
2024-01-05 19:30:50 +0300

    feat: add reauth flow
    add async mutex

--- src/api/base-api.ts
+++ src/api/base-api.ts
@@ -1,13 +1,9 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi } from '@reduxjs/toolkit/query/react'
+
+import { baseQueryWithReauth } from './base-query.reauth'
 
 export const baseApi = createApi({
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'https://api.flashcards.andrii.es',
-    credentials: 'include',
-    prepareHeaders: headers => {
-      headers.append('x-auth-skip', 'true')
-    },
-  }),
+  baseQuery: baseQueryWithReauth,
   endpoints: () => ({}),
   reducerPath: 'baseApi',
   tagTypes: ['Decks', 'Cards', 'Me'],
