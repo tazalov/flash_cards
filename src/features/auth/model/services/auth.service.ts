14de1c4af45afe67089460ea1d380d3fc4bc3b2c
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2024-01-07 19:11:57 +0300

    feat: add login, recoverPassword and reset password in auth.service

--- src/features/auth/model/services/auth.service.ts
+++ src/features/auth/model/services/auth.service.ts
@@ -1,16 +1,50 @@
 import { baseApi } from '@/api'
+import { SignInFormData } from '@/features/auth/model/hooks/useSignInForm'
 
-import { User } from '../types/service.types'
+import { LoginResponse, User } from '../types/service.types'
 
 const authService = baseApi.injectEndpoints({
   endpoints: builder => {
     return {
+      login: builder.mutation<LoginResponse, SignInFormData>({
+        invalidatesTags: ['Me'],
+        query: body => ({
+          body,
+          method: 'POST',
+          url: '/v1/auth/login',
+        }),
+      }),
       me: builder.query<User, void>({
         providesTags: ['Me'],
         query: () => ({ url: '/v1/auth/me' }),
+      }),
+      recoverPassword: builder.mutation<void, string>({
+        query: email => ({
+          body: {
+            email,
+            html: '<h1>Hi, ##name##</h1><p>Click <a href="http://localhost:5173/create-new-password/##token##">here</a> to create new password</p>',
+            // subject: 'Recover password',
+          },
+          method: 'POST',
+          url: '/v1/auth/recover-password',
+        }),
+      }),
+      resetPassword: builder.mutation<void, { password: string; token: string }>({
+        query: ({ password, token }) => ({
+          body: {
+            password,
+          },
+          method: 'POST',
+          url: `/v1/auth/reset-password/${token}`,
+        }),
       }),
     }
   },
 })
 
-export const { useMeQuery } = authService
+export const {
+  useLoginMutation,
+  useMeQuery,
+  useRecoverPasswordMutation,
+  useResetPasswordMutation,
+} = authService
--- src/features/auth/model/types/service.types.ts
+++ src/features/auth/model/types/service.types.ts
@@ -7,3 +7,7 @@ export interface User {
   name: string
   updated: string
 }
+
+export type LoginResponse = {
+  accessToken: string
+}
