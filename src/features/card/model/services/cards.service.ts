2d68f5aafbbbf7c168b20b84d3361f81b2d2b3fe
tazalov <tazalov9@gmail.com>
2024-01-10 10:43:17 +0300

    feat: add card services to learn

--- src/features/card/model/services/cards.service.ts
+++ src/features/card/model/services/cards.service.ts
@@ -1,11 +1,34 @@
 import { baseApi } from '@/api'
 
 import { Card } from '../types/cards.types'
-import { GetCardsArgs, GetCardsResponse } from '../types/service.types'
+import {
+  ChangeGradeCardArgs,
+  GetCardsArgs,
+  GetCardsResponse,
+  GetRandomCardArgs,
+} from '../types/service.types'
 
 const cardsService = baseApi.injectEndpoints({
   endpoints: builder => {
     return {
+      changeGradeCard: builder.mutation<Card, ChangeGradeCardArgs>({
+        async onQueryStarted({ deckId }, { dispatch, queryFulfilled }) {
+          try {
+            const { data: nextCard } = await queryFulfilled
+
+            dispatch(
+              cardsService.util.updateQueryData('getRandomCard', { id: deckId }, () => nextCard)
+            )
+          } catch (error) {
+            console.log(error)
+          }
+        },
+        query: ({ deckId, ...restArgs }) => ({
+          body: restArgs,
+          method: 'POST',
+          url: `v1/decks/${deckId}/learn`,
+        }),
+      }),
       createCard: builder.mutation<Card, { body: FormData; id: string }>({
         invalidatesTags: ['Cards'],
         query: ({ body, id }) => ({
@@ -18,6 +41,12 @@ const cardsService = baseApi.injectEndpoints({
         providesTags: ['Cards'],
         query: ({ id, params }) => ({ params, url: `v1/decks/${id}/cards` }),
       }),
+      getRandomCard: builder.query<Card, GetRandomCardArgs>({
+        query: ({ id, previousCardId }) => ({
+          params: { previousCardId },
+          url: `v1/decks/${id}/learn`,
+        }),
+      }),
       removeCard: builder.mutation<Card, { id: string }>({
         invalidatesTags: ['Cards'],
         query: ({ id }) => ({ method: 'DELETE', url: `v1/cards/${id}` }),
@@ -26,4 +55,10 @@ const cardsService = baseApi.injectEndpoints({
   },
 })
 
-export const { useCreateCardMutation, useGetCardsByIdQuery, useRemoveCardMutation } = cardsService
+export const {
+  useChangeGradeCardMutation,
+  useCreateCardMutation,
+  useGetCardsByIdQuery,
+  useGetRandomCardQuery,
+  useRemoveCardMutation,
+} = cardsService
