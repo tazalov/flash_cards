f9abff22444e48bf6bd6721841760c86f85d8936
Vladimir Zvyagin <vladimir_zvyagin@mail.ru>
2024-01-10 07:40:06 +0300

    feat: added UpdateCardModal.tsx component

---
+++ src/features/card/ui/CardActions/UpdateCardModal/UpdateCardModal.tsx
@@ -0,0 +1,54 @@
+import { ReactNode, useState } from 'react'
+
+import { Edit } from '@/common/assets/icons'
+import { IconButton } from '@/common/ui/IconButton'
+import { Modal } from '@/common/ui/Modals'
+import { Card } from '@/features/card'
+import cn from 'classnames'
+
+import s from './UpdateCardModal.module.scss'
+
+import { useUpdateCardMutation } from '../../../model/services/cards.service'
+import { CreateCardForm } from '../CreateCardForm/CreateCardForm'
+
+type Props = {
+  card: Card
+  className?: string
+  trigger?: ReactNode
+}
+
+export const UpdateCardModal = ({
+  card,
+  className,
+  trigger = <IconButton icon={<Edit />} />,
+}: Props) => {
+  const [open, setOpen] = useState(false)
+
+  const [update, { isLoading }] = useUpdateCardMutation()
+  const handleUpdateCard = (formValues: FormData) => {
+    update({ body: formValues, id: card.id })
+      .unwrap()
+      .then(data => {
+        if (data) {
+          setOpen(false)
+        }
+      })
+  }
+
+  return (
+    <Modal
+      className={cn(s.modal, className)}
+      onOpenChange={setOpen}
+      open={open}
+      title="Update Card"
+      trigger={trigger}
+    >
+      <CreateCardForm
+        card={card}
+        isLoading={isLoading}
+        onSubmit={handleUpdateCard}
+        submitTitle="Update Card"
+      />
+    </Modal>
+  )
+}
