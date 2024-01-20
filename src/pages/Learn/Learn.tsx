ba5eba08dfeb0e6bbb7f507c2dd1dbab5705de48
tazalov <tazalov9@gmail.com>
2024-01-20 14:18:17 +0300

    refactor: show skeleton flow, add catching errors

--- src/pages/Learn/Learn.tsx
+++ src/pages/Learn/Learn.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 import { Arrow } from '@/common/assets/icons'
 import { TypographyVariant } from '@/common/enums'
 import { Page } from '@/common/ui/Page'
 import { Preloader } from '@/common/ui/Preloader'
 import { Typography } from '@/common/ui/Typography'
+import { handleErrorResponse } from '@/common/utils'
 import {
   CardBody,
   CardBodySkeleton,
@@ -23,14 +25,24 @@ export const Learn = () => {
 
   const [hideAnswer, setHideAnswer] = useState(true)
 
-  const { data, isLoading: isLoadRandomCard } = useGetRandomCardQuery({ id: deckId })
+  const {
+    data,
+    isError: isErrorGetCard,
+    isLoading: isLoadRandomCard,
+  } = useGetRandomCardQuery({ id: deckId })
 
-  const [changeGrade, { isLoading }] = useChangeGradeCardMutation()
+  const [changeGrade, { isError: isErrorChangeGrade, isLoading: isLoadChangeGrade }] =
+    useChangeGradeCardMutation()
 
   const handleSubmitGrade = (formData: Rate) => {
-    changeGrade({ cardId: data!.id, deckId, grade: Number(formData.grade) })
-      .unwrap()
-      .then(() => setHideAnswer(true))
+    changeGrade({ cardId: data!.id, deckId, grade: Number(formData.grade) }).then(data => {
+      if ('error' in data) {
+        return handleErrorResponse(data.error)
+      } else {
+        toast.success(`Grade has been successfully counted!`)
+        setHideAnswer(true)
+      }
+    })
   }
 
   const handleShowAnswer = () => setHideAnswer(false)
@@ -45,13 +57,13 @@ export const Learn = () => {
         <Arrow className={s.iconArrow} />
         <Typography variant={TypographyVariant.body2}>Back to previous page</Typography>
       </div>
-      {isLoading ? (
+      {isLoadChangeGrade || isErrorGetCard || isErrorChangeGrade ? (
         <CardBodySkeleton />
       ) : (
         <CardBody
           card={data}
           deckName={deckName}
-          disabled={isLoading}
+          disabled={isLoadChangeGrade}
           handleShowAnswer={handleShowAnswer}
           hideAnswer={hideAnswer}
           onSubmit={handleSubmitGrade}
