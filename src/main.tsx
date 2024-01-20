a62cbc7a66cc9627616883e89f28a7caf90d1fd1
tazalov <tazalov9@gmail.com>
2024-01-20 13:40:43 +0300

    refactor: move toast container to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { Bounce, ToastContainer } from 'react-toastify'
 
 import { AppRouter } from '@/app/routes'
 import { store } from '@/app/store'
@@ -14,6 +15,19 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <AppRouter />
+      <ToastContainer
+        autoClose={5000}
+        closeOnClick
+        draggable={false}
+        hideProgressBar={false}
+        newestOnTop
+        pauseOnFocusLoss={false}
+        pauseOnHover
+        position="bottom-left"
+        rtl={false}
+        theme="dark"
+        transition={Bounce}
+      />
     </Provider>
   </React.StrictMode>
 )
