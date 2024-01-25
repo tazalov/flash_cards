5725e9cf78a734f0da256f76bbbffac7e5162798
tazalov <tazalov9@gmail.com>
2024-01-25 08:03:14 +0300

    fix: Layout stories

--- src/layout/Header/Header.stories.tsx
+++ src/layout/Header/Header.stories.tsx
@@ -4,17 +4,25 @@ import { Header } from './Header'
 
 const meta = {
   argTypes: {
-    isAuth: {
+    data: {
       control: false,
+      description: 'Current auth user data',
+    },
+    isAuth: {
+      control: 'boolean',
+      description: 'Flag for render profile/sign-in button',
     },
     logout: {
-      action: 'Logout!',
+      action: 'Success logout',
+      control: false,
+      description: 'Callback for logout user',
     },
   },
   component: Header,
   parameters: {
     layout: 'fullscreen',
   },
+  tags: ['autodocs'],
   title: 'layout/Header',
 } satisfies Meta<typeof Header>
 
@@ -22,19 +30,22 @@ export default meta
 
 type Story = StoryObj<typeof meta>
 
+const userData = {
+  avatar: 'https://cojo.ru/wp-content/uploads/2022/12/menedzher-illiustratsiia-2.webp',
+  email: 'some@email.com',
+  username: 'MyUsername',
+}
+
 export const Auth: Story = {
   args: {
-    data: {
-      avatar: 'https://cojo.ru/wp-content/uploads/2022/12/menedzher-illiustratsiia-2.webp',
-      email: 'some@email.com',
-      username: 'MyUsername',
-    },
+    data: userData,
     isAuth: true,
   },
 }
 
 export const NoAuth: Story = {
   args: {
+    data: userData,
     isAuth: false,
   },
 }
