c08545a61e0247f3056ca660e22071c56e531261
ruslan <zrm1306@gmail.com>
2024-01-25 12:12:32 +0500

    refactor: added const for columns deck/card table

--- src/common/const/index.ts
+++ src/common/const/index.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod'
 
+import { Column } from '../types'
+
 export const ALLOWED_IMAGES_FORMATS = ['image/jpeg', 'image/jpg', 'image/png', 'image/webp']
 export const MAX_SIZE_IMAGE = 1000000
 
@@ -15,3 +17,63 @@ export const COVER_SCHEMA = z
   )
 
 export const ON_SUBMIT_SB = () => Promise.resolve({ error: null, fieldErrors: null })
+
+export const PAGINATION_OPTIONS = [
+  { title: '5', value: '5' },
+  { title: '10', value: '10' },
+  { title: '20', value: '20' },
+  { title: '30', value: '30' },
+]
+
+export const CARDS_COLUMNS: Column[] = [
+  {
+    key: 'question',
+    sortable: true,
+    title: 'Question',
+  },
+  {
+    key: 'answer',
+    sortable: true,
+    title: 'Answer',
+  },
+  {
+    key: 'updated',
+    sortable: true,
+    title: 'Last Updated',
+  },
+  {
+    key: 'grade',
+    sortable: true,
+    title: 'Grade',
+  },
+  {
+    key: 'buttons',
+    sortable: false,
+  },
+]
+export const DECKS_COLUMNS: Column[] = [
+  {
+    key: 'name',
+    sortable: true,
+    title: 'Name',
+  },
+  {
+    key: 'cardsCount',
+    sortable: true,
+    title: 'Cards',
+  },
+  {
+    key: 'updated',
+    sortable: true,
+    title: 'Last Updated',
+  },
+  {
+    key: 'created',
+    sortable: true,
+    title: 'Created by',
+  },
+  {
+    key: 'buttons',
+    sortable: false,
+  },
+]
